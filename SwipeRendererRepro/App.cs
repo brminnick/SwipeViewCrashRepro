88e999e200ee5b4a4a457c49408749180f756fb5
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-01-06 20:37:28 -0800

    First Commit

---
+++ SwipeRendererRepro/App.cs
@@ -0,0 +1,57 @@
+﻿using Xamarin.Forms;
+
+namespace SwipeRendererRepro
+{
+    public class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new[] { "SwipeView_Experimental" });
+            MainPage = new MainPage();
+        }
+    }
+
+    class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var label = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Text = "Swipe Right-to-Left to Crash App\n\nSwipe Left-to-Right for Working Example",
+                BackgroundColor = Color.LightGoldenrodYellow
+            };
+
+            var swipeItemWithText = new SwipeItem
+            {
+                IconImageSource = "AppIcon",
+                BackgroundColor = Color.Green,
+                Text = "Test"
+            };
+
+            var swipeItemWithNoText = new SwipeItem
+            {
+                IconImageSource = "AppIcon",
+                BackgroundColor = Color.Red
+            };
+
+            var swipeView = new SwipeView
+            {
+                Margin = new Thickness(20),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                LeftItems = { swipeItemWithText },
+                RightItems = { swipeItemWithNoText },
+                Content = label
+            };
+
+            Content = new ScrollView
+            {
+                Content = swipeView
+            };
+        }
+    }
+}
